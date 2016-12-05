ce58089494294f503d42bb5f108b8f3f8cb55b45
cFrost <cFrost@cfrost.net>
2016-12-05 23:50:43 +0800

    eulertable

--- resources/js/lib/euler-ui.js
+++ resources/js/lib/euler-ui.js
@@ -18,185 +18,303 @@ $.fn.serializeJson=function(){
     return serializeObj;
 };
 
-$(function(){
-    var eulerTables = $('.euler-table');
-    eulerTables.each(function () {
-        initEulerTable(this);
-        loadData(this);
-    })
-});
-
-function initEulerTable(table) {
-    var tbody = '<tbody></tbody>'
-    $(table).append(tbody);
-
-    var option = getOption($(table).data('option'));
-
-    var toolbar = option.toolbar;
-    console.log(toolbar);
-    $(toolbar).insertBefore(table);
-
-    if(option.pagination == 'true') {
-        var pageBtnhtml = '<nav class="euler-table-page-btn">'
-            + '<ul class="pagination">'
-            + '</ul>'
-            + '</nav>';
-        $(pageBtnhtml).insertAfter(table);
-    }
-}
-
-function loadData(table, queryData) {
-    var option = getOption($(table).data('option'));
-
-    var action = option.action;
-
-    var data = {};
-    if(typeof(queryData) != 'undefined') {
-        data = queryData;
-    }
-
-    if(option.pagination == 'true') {
-        var pageBtn = $(table).next('.euler-table-page-btn').children('ul');
-        var currPage = $(pageBtn).children('.active').children('a').html();
-        if(typeof(currPage) == 'undefined') {
-            currPage = 1;
-        }
-        data.pageIndex = currPage;
-        data.pageSize = 10;
-    }
-
-    data._r = new Date().getTime();
-
-    $.ajax({
-        url: action,
-        type:'GET',
-        async:true,
-        data: data,
-        error:function(XMLHttpRequest, textStatus, errorThrown) {
+$.fn.eulertable = function (optionObj) {
+    var table = this;
+    var option = optionObj;
+    var rows;
+    var pages;
+    var pageIndex;
+    var pageSize;
+    var total;
+    var that;
+    var query;
+    return {
+        init: function () {
+            var htmlOption = getOption($(table).data('option'));
+            option = $.extend({}, option, htmlOption);
+            that = this;
+            initEulerTable(table, option);
         },
-        success:function(data, textStatus) {
-            var ths = $(table).find('th');
-            var html = "";
-            for(var i in data.rows) {
-                var row = data.rows[i];
-                html += '<tr>';
-                html += '<input type="hidden" value="'+i+'">';
-                ths.each(function () {
-
-                    var field = $(this).data('field');
-                    var value;
-                    if(typeof(field) != 'undefined') {
-                        value = row[field];
-                    }
-                    var formatter = $(this).data('formatter');
-                    if(typeof(formatter) != 'undefined') {
-                        value = eval(formatter).call(formatter, value, row, i);
-                    }
-                    if(typeof(value) == 'undefined')
-                        value = '';
-
-                    var optionTh = getOption($(this).data('option'));
-
-                    if(optionTh.checkbox == 'true') {
-                        var keyField = optionTh.checkboxKey;
-                        var key;
-                        if(typeof(keyField) != 'undefined') {
-                            key = row[keyField];
-                        }
-                        if(typeof(key) == 'undefined')
-                            key = '';
-
-                        html += '<td><input type="checkbox" value="'+key+'"> ' + value + '</td>';
-                    } else if($(this).hasClass('euler-dashboard-user-action-th')) {
-                        html += '<td class="euler-user-action-td">';
-                        if(row.enabled == true)
-                            html +='<button type="button" class="btn btn-danger btn-user-action">Block</button>\n';
-                        else
-                            html +='<button type="button" class="btn btn-danger btn-user-action" disabled="disabled">Block</button>\n';
-                        html +='<button type="button" class="btn btn-default btn-user-action">Edit</button>'
-                            +'</td>';
-                    } else {
-                        if(typeof(value) == 'undefined')
-                            value = '-';
-                        html += '<td>' + value + '</td>';
-                    }
-
-                });
-                html += '</tr>'
-            }
-            $(table).find('tbody').html(html);
+        refreshData: function(queryObj) {
+            if (typeof(queryObj) != 'undefined')
+                query = queryObj;
+            else
+                queryObj = query;
 
             if(option.pagination == 'true') {
-                var pageIndex = data.pageIndex;
-                var pageSize = data.pageSize;
-                var total = data.total;
-                var pages = parseInt(total / pageSize);
-                if (total % pageSize > 0) pages++;
+                if (typeof(queryObj) != 'object')
+                    queryObj = {};
 
-                wrapPageBtn(pageBtn, pageIndex, pages);
+                queryObj.pageIndex = pageIndex;
+                queryObj.pageSize = pageSize;
+
+                if (typeof(queryObj.pageIndex) == 'undefined')
+                    queryObj.pageIndex = 1;
+                if (typeof(queryObj.pageSize) == 'undefined')
+                    queryObj.pageSize = 10;
             }
+
+            loadData(queryObj, function(data, textStatus) {
+                rows = data.rows;
+
+                wrapData(rows);
+
+                if(option.pagination == 'true') {
+                    pageIndex = data.pageIndex;
+                    pageSize = data.pageSize;
+                    total = data.total;
+                    pages = parseInt(total / pageSize);
+                    if(total % pageSize > 0) pages++;
+
+                    var pageBtn = $(table).next('.euler-table-page-btn').children('ul');
+                    wrapPageBtn(pageBtn, pageIndex, pages, that);
+                }
+            });
+        },
+        getData: function () {
+            return rows;
+        },
+        getOption: function () {
+            return option;
+        },
+        nextPage: function () {
+            var queryObj = query;
+            if (typeof(queryObj) != 'object')
+                queryObj = {};
+            queryObj.pageIndex = pageIndex + 1;
+            queryObj.pageSize = pageSize;
+
+
+            loadData(queryObj, function(data, textStatus) {
+                rows = data.rows;
+
+                wrapData(rows);
+
+                if(option.pagination == 'true') {
+                    pageIndex = data.pageIndex;
+                    pageSize = data.pageSize;
+                    total = data.total;
+                    pages = parseInt(total / pageSize);
+                    if(total % pageSize > 0) pages++;
+
+                    var pageBtn = $(table).next('.euler-table-page-btn').children('ul');
+                    wrapPageBtn(pageBtn, pageIndex, pages, that);
+                }
+            });
+
+        },
+        prePage: function () {
+            var queryObj = query;
+            if (typeof(queryObj) != 'object')
+                queryObj = {};
+            queryObj.pageIndex = pageIndex - 1;
+            queryObj.pageSize = pageSize;
+
+
+            loadData(queryObj, function(data, textStatus) {
+                rows = data.rows;
+
+                wrapData(rows);
+
+                if(option.pagination == 'true') {
+                    pageIndex = data.pageIndex;
+                    pageSize = data.pageSize;
+                    total = data.total;
+                    pages = parseInt(total / pageSize);
+                    if(total % pageSize > 0) pages++;
+
+                    var pageBtn = $(table).next('.euler-table-page-btn').children('ul');
+                    wrapPageBtn(pageBtn, pageIndex, pages, that);
+                }
+            });
+
+        },
+        goPage: function (pageNum) {
+            var queryObj = query;
+            if (typeof(queryObj) != 'object')
+                queryObj = {};
+            queryObj.pageIndex = pageNum;
+            queryObj.pageSize = pageSize;
+
+
+            loadData(queryObj, function(data, textStatus) {
+                rows = data.rows;
+
+                wrapData(rows);
+
+                if(option.pagination == 'true') {
+                    pageIndex = data.pageIndex;
+                    pageSize = data.pageSize;
+                    total = data.total;
+                    pages = parseInt(total / pageSize);
+                    if(total % pageSize > 0) pages++;
+
+                    var pageBtn = $(table).next('.euler-table-page-btn').children('ul');
+                    wrapPageBtn(pageBtn, pageIndex, pages, that);
+                }
+            });
+
         }
-    });
-}
-
-function getOption(str) {
-    if(typeof(str) == 'undefined')
-        return {};
-
-    var untiArray = str.replace(/[\s]/g, '').split(',');
-
-    var result = {};
-    for(var i in untiArray) {
-        var unti = untiArray[i];
-        var key = unti.substr(0, unti.indexOf(':'));
-        var value = unti.substr(unti.indexOf(':')+1);
-
-        if((value.startsWith("'") && value.endsWith("'")) || (value.startsWith('"') && value.endsWith('"')))
-            value = value.substr(1, value.length - 2)
-
-        result[key] = value;
     }
 
-    return result;
-}
 
-function wrapPageBtn(pageBtn, pageIndex, pages) {
-    var html = '<li><a href="#">&laquo;</a></li>';
 
-    if(pages <= 10) {
-        for(var i = 1; i < pages+1; i++) {
-            html += '<li class="page-'+i+'"><a href="javascript:void(0)" onclick="clickPage(this)">'+i+'</a></li>';
+
+    function initEulerTable(table, option) {
+
+        var tbody = '<tbody></tbody>'
+        $(table).append(tbody);
+
+        var toolbar = option.toolbar;
+        $(toolbar).insertBefore(table);
+
+        if(option.pagination == 'true') {
+            var pageBtnhtml = '<nav class="euler-table-page-btn">'
+                + '<ul class="pagination">'
+                + '</ul>'
+                + '</nav>';
+            $(pageBtnhtml).insertAfter(table);
         }
-    } else {
-        if(pageIndex <= 5) {
-            for(var i = 1; i < 11; i++) {
-                html += '<li class="page-'+i+'"><a href="javascript:void(0)" onclick="clickPage(this)">'+i+'</a></li>';
+    }
+
+    function loadData(queryData, callback) {
+        var action = option.action;
+
+        var data = {};
+        if(typeof(queryData) != 'undefined') {
+            data = queryData;
+        }
+
+        data._r = new Date().getTime();
+
+        $.ajax({
+            url: action,
+            type:'GET',
+            async:true,
+            data: data,
+            error:function(XMLHttpRequest, textStatus, errorThrown) {
+            },
+            success:function(data, textStatus) {
+                callback(data, textStatus);
+            }
+        });
+    }
+
+    function getOption(str) {
+        if(typeof(str) == 'undefined')
+            return {};
+
+        var untiArray = str.replace(/[\s]/g, '').split(',');
+
+        var result = {};
+        for(var i in untiArray) {
+            var unti = untiArray[i];
+            var key = unti.substr(0, unti.indexOf(':'));
+            var value = unti.substr(unti.indexOf(':')+1);
+
+            if((value.startsWith("'") && value.endsWith("'")) || (value.startsWith('"') && value.endsWith('"')))
+                value = value.substr(1, value.length - 2)
+
+            result[key] = value;
+        }
+
+        return result;
+    }
+
+    function wrapData(rows) {
+        var ths = $(table).find('th');
+        var html = "";
+
+        for(var i in rows) {
+            var row = rows[i];
+            html += '<tr>';
+            html += '<input type="hidden" value="'+i+'">';
+            ths.each(function () {
+
+                var field = $(this).data('field');
+                var value;
+
+                if(typeof(field) != 'undefined') {
+                    value = row[field];
+                } else {
+                    value = '';
+                }
+
+                var formatter = $(this).data('formatter');
+                if(typeof(formatter) != 'undefined') {
+                    value = eval(formatter).call(formatter, value, row, i);
+                }
+                if(typeof(value) == 'undefined')
+                    value = null;
+
+                var optionTh = getOption($(this).data('option'));
+
+                html += '<td>';
+
+                if(optionTh.checkbox == 'true') {
+                    var keyField = optionTh.checkboxKey;
+                    var key;
+                    if(typeof(keyField) != 'undefined') {
+                        key = row[keyField];
+                    }
+                    if(typeof(key) == 'undefined')
+                        key = '';
+
+                    html += '<input type="checkbox" value="'+key+'"> ';
+                }
+
+                html += value + '</td>';
+
+            });
+            html += '</tr>'
+        }
+        $(table).find('tbody').html(html);
+    }
+
+    function wrapPageBtn(pageBtn, pageIndex, pages, that) {
+        var html = '<li><a href="javascript:void(0)" onclick="prePage()">&laquo;</a></li>';
+
+        if(pages <= 10) {
+            for(var i = 1; i < pages+1; i++) {
+                html += '<li class="page-'+i+'"><a href="javascript:void(0)" onclick="goPage('+i+')">'+i+'</a></li>';
             }
         } else {
-            html += '<li class="page-1"><a href="javascript:void(0)" onclick="clickPage(this)">1</a></li>';
-            html += '<li><span>...</span></li>';
-            var start = pageIndex - 3;
-            var end = pageIndex + 4;
-            for(var i = start; i <= end; i++) {
-                html += '<li class="page-'+i+'"><a href="javascript:void(0)" onclick="clickPage(this)">'+i+'</a></li>';
+            if(pageIndex <= 5) {
+                for(var i = 1; i < 11; i++) {
+                    html += '<li class="page-'+i+'"><a href="javascript:void(0)" onclick="goPage('+i+')">'+i+'</a></li>';
+                }
+            } else {
+                html += '<li class="page-1"><a href="javascript:void(0)" onclick="goPage(1)">1</a></li>';
+                html += '<li><span>...</span></li>';
+                var start = pageIndex - 3;
+                var end = pageIndex + 4;
+                for(var i = start; i <= end; i++) {
+                    html += '<li class="page-'+i+'"><a href="javascript:void(0)" onclick="goPage('+i+')">'+i+'</a></li>';
+                }
             }
+            html += '<li><span>...</span></li>';
+            html += '<li class="page-'+pages+'"><a href="javascript:void(0)" onclick="goPage('+pages+')">'+pages+'</a></li>';
         }
-        html += '<li><span>...</span></li>';
-        html += '<li class="page-'+pages+'"><a href="javascript:void(0)" onclick="clickPage(this)">'+pages+'</a></li>';
+        html += '<li><a href="javascript:void(0)" onclick="nextPage()">&raquo;</a></li>';
+
+        pageBtn.html(html);
+
+        $($(pageBtn).children('.page-'+pageIndex)).addClass('active');
     }
-    html += '<li><a href="#">&raquo;</a></li>';
+};
 
-    pageBtn.html(html);
 
-    $($(pageBtn).children('.page-'+pageIndex)).addClass('active');
-}
-
-function clickPage(data) {
-    $(data).parent().parent().children('.active').removeClass('active');
-    $(data).parent().addClass('active');
-    loadData($(data).parent().parent().parent().parent());
-}
 
 function activeFormatter(value,row,index){
     return value == true ? 'active': 'blocked';
+}
+
+function userActionBtnFormatter(value,row,index){
+    var html = '<span style="float: right">';
+    if(row.enabled == true)
+        html +='<button type="button" class="btn btn-danger btn-user-action">Block</button>\n';
+    else
+        html +='<button type="button" class="btn btn-danger btn-user-action" disabled="disabled">Block</button>\n';
+    html +='<button type="button" class="btn btn-default btn-user-action">Edit</button></span>'
+    return html;
 }
